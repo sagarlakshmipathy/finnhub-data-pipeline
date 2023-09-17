e080390f95e3edbb1657d149f9acbb822d006921
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-09-16 23:36:04 -0700

    moving all files from main

--- src/kafka_to_s3_raw.py
+++ src/kafka_to_s3_raw.py
@@ -13,13 +13,13 @@ def read_from_kafka(spark_session):
         .load()
 
 
-def write_to_s3_raw(spark_session, bucket_name):
+def write_to_s3_raw(spark_session, bucket_name, source_zone, target_zone, output_format, output_mode):
     raw_df = read_from_kafka(spark_session)
     df_with_schema = raw_df \
         .select(from_json(col("value").cast(StringType()), finnhub_schema).alias("data")) \
         .select("data.*")
 
-    write_to_s3(df_with_schema, bucket_name, "raw")
+    write_to_s3(df_with_schema, bucket_name, target_zone, output_format, output_mode)
     # write_to_console_append(df_with_schema)
 
 
@@ -31,4 +31,11 @@ if __name__ == '__main__':
         .master("local[2]") \
         .getOrCreate()
 
-    write_to_s3_raw(spark, "finnhub-data-pipeline")
+    # write_to_s3_raw(spark, "finnhub-data-pipeline")
+    write_to_s3_raw(
+        spark,
+        "finnhub-data-pipeline",
+        "kafka",
+        "raw",
+        "json",
+        "append")
--- src/raw_to_bronze.py
+++ src/raw_to_bronze.py
@@ -6,7 +6,7 @@ from src.common.utils import *
 from src.common.schema import *
 
 
-def write_to_s3_bronze(spark_session, schema, bucket_name, source_zone, target_zone, output_mode):
+def write_to_s3_bronze(spark_session, schema, bucket_name, source_zone, target_zone, output_format, output_mode):
     df_with_schema = read_from_s3(spark_session, schema, bucket_name, source_zone)
 
     bronze_df = df_with_schema.select(
@@ -17,7 +17,7 @@ def write_to_s3_bronze(spark_session, schema, bucket_name, source_zone, target_z
         col("t").cast(LongType()).alias("timestamp")
     )
 
-    write_to_s3(bronze_df, bucket_name, target_zone, output_mode)
+    write_to_s3(bronze_df, bucket_name, target_zone, output_format, output_mode)
     # write_to_console_append(bronze_df)
 
 
@@ -29,4 +29,10 @@ if __name__ == '__main__':
         .master("local[2]") \
         .getOrCreate()
 
-    write_to_s3_bronze(spark, finnhub_schema, "finnhub-data-pipeline", "raw", "bronze", "append")
+    write_to_s3_bronze(spark,
+                       finnhub_schema,
+                       "finnhub-data-pipeline",
+                       "raw",
+                       "bronze",
+                       "json",
+                       "append")
